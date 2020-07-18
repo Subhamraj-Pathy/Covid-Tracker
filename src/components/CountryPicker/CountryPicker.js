e6d4851a8b0527b347db5725af192afded6ed766
Subhamraj-Pathy <subhamrajpathy@gmail.com>
2020-07-18 09:56:40 +0530

    Country Drop Down

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FormControl, NativeSelect } from '@material-ui/core'
+import styles from './CountryPicker.module.css'
+import { fetchCountries } from '../../api'
 
 const CountryPicker = () => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([])
+
+    useEffect(() => {
+        const fetchCountriesFromAPI = async () => {
+            setFetchedCountries(await fetchCountries())
+        }
+        fetchCountriesFromAPI()
+    }, [setFetchedCountries])
+
+    console.log(fetchedCountries)
+
     return (
-        <div>
-            Country Picker
-        </div>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value='global'>Global</option>
+                {fetchedCountries.map((country,i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     );
 };
 
