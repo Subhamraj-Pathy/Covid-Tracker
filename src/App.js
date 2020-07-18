b0f038558c41fe8c161b8bafd609ba7aa67c3251
Subhamraj-Pathy <subhamrajpathy@gmail.com>
2020-07-18 09:03:18 +0530

    Display Cards

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ import styles from './App.module.css'
 
 class App extends Component {
 
+    state = {
+        data: {}
+    }
+
     async componentDidMount() {
-        const data = await fetchData()
-        console.log(data);
+        const fetchedData = await fetchData()
+        this.setState({data: fetchedData})
     }
 
     render() {
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={this.state.data} />
                 <CountryPicker />
                 <Chart />
             </div>
